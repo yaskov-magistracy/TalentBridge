42ddbd6a77666f1415b99b2929828604283e006f
yaskov <yaskovanton333@gmail.com>
2026-05-05 22:29:54 +0400

    ErrorHandling + PingController

---
+++ src/PublicAPI/API/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PingController : ControllerBase
+{
+    [HttpGet("health")]
+    public async Task<ActionResult> Ping()
+    {
+        return Ok("Всё ок");
+    }
+
+    [HttpPost("error")]
+    public async Task<ActionResult> PingError()
+    {
+        throw new Exception("Упала ошибка");
+    }
+}
